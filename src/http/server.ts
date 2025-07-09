54a5c3d1ec7d07ae051e06672d6c7f0113f840af
Gilson Nunes <GilsonNunes_dev@outlook.com>
2025-07-09 17:47:42 -0300

    feat(server): setup http server

---
+++ src/http/server.ts
@@ -0,0 +1,25 @@
+import fastifyCors from "@fastify/cors";
+import { fastify } from "fastify";
+import {
+	type ZodTypeProvider,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
+
+const app = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+	origin: env.FRONTEND_URL,
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get("/health", async () => {
+	return { status: "ok" };
+});
+
+app.listen({ port: env.PORT }).then((address) => {
+	console.log(`Server listening at ${address}`);
+});
